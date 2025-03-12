fc501bb75f609256b52b53b4f50a8b91706f1e31
Muhammad Attiq <m.attiq.ce@gmail.com>
2025-03-12 07:18:20 -0700

    Create Scaling_Shifting_Reversal_2.m

---
+++ Signal_Transformations/Scaling_Shifting_Reversal_2.m
@@ -0,0 +1,24 @@
+function y = sequence(x, n, n0, a, b) 
+    x = x(:); 
+    n = n(:); 
+    subplot(5, 1, 1); 
+    stem(n, x, 'LineWidth', 2); 
+    title('Original Sequence'); grid on; 
+    subplot(5, 1, 2); 
+    stem(n + n0, x, 'LineWidth', 2); 
+    title('Shifted Sequence'); grid on; 
+    subplot(5, 1, 3); 
+    stem(-n, x, 'LineWidth', 2); 
+    title('Time-Reversed Sequence');  grid on; 
+    x1 = downsample(x, a); 
+    n1 = n(1:a:end); 
+    subplot(5, 1, 4); 
+    stem(n1, x1, 'LineWidth', 2); 
+    title('Downsampled Sequence');  grid on; 
+    x2 = upsample(x, b); 
+    i = (length(x2) - 1) / 2; 
+    n2 = -i:i; 
+    subplot(5, 1, 5); 
+    stem(n2, x2, 'LineWidth', 2); 
+    title('Upsampled Sequence'); 
+    grid on;
